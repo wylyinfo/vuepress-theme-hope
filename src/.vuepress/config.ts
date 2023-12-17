da518b03a6eb0e01ad788bcb88984e853a00ee49
wuyanluoye <refersearch@163.com>
2023-12-17 20:02:19 +0800

    feat: workflow触发测试

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from "./theme.js";
 import {searchProPlugin} from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/vuepress-theme-hope/",
 
   lang: "zh-CN",
   title: "博客演示",
